ba6d86d2ccca6b8cc6545e30a76b7a63b0cb3376
KUSHAGRA JINDAL <kushagra2jindal@gmail.com>
2020-12-30 15:29:01 +0530

    Naive Bayes Classifier created

---
+++ Positive-Negative/src/classifier.py
@@ -0,0 +1,22 @@
+from DataPreprocessing import dataProcessing
+from nltk import NaiveBayesClassifier
+from nltk.tokenize import word_tokenize
+
+from NLPFunctions import remove_noise
+
+processedData = dataProcessing()
+
+classifier = NaiveBayesClassifier.train(processedData)
+
+
+'''
+random model testing!!!!!!!!!!!!
+'''
+
+choice = 'y'
+
+while (choice == 'y'):
+    custom_tweet = input("Enter your comment : ") 
+    custom_tokens = remove_noise(word_tokenize(custom_tweet))
+    print(classifier.classify(dict([token, True] for token in custom_tokens)))
+    choice = input("press y to continue n to exit ") 
